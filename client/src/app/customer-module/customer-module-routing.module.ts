78e3a9d926fd70e7f93b49494a1a984655ef4c1b
Hoang Binh <hbinhit221@gmail.com>
2022-11-03 23:22:42 +0700

    Complete config customer layout

--- client/src/app/customer-module/customer-module-routing.module.ts
+++ client/src/app/customer-module/customer-module-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerViewComponent } from './customer-view/customer-view.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {
+  path: '',
+  component: CustomerViewComponent
+ },
+ {
+  path: 'register',
+  component: RegisterComponent
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
